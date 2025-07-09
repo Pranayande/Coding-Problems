33d6b8ed6ddfab9bb5a8a66ff1ea4c007a94e220
Pranayande <165485309+Pranayande@users.noreply.github.com>
2025-07-09 23:20:38 +0530

    Create QuickSort.java
    This program implements the Quick Sort algorithm using the Hoare partition scheme in Java. It sorts an array of integers in ascending order by selecting a pivot element and recursively partitioning the array around it. Quick Sort is an efficient, in-place, divide-and-conquer sorting algorithm with an average time complexity of O(n log n). This implementation demonstrates fundamental concepts of recursion, array manipulation, and partitioning logic.

---
+++ Leet Code/QuickSort.java
@@ -0,0 +1,34 @@
+class Solution {
+    static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pIndex = partition(arr, low, high);
+            quickSort(arr, low, pIndex - 1);
+            quickSort(arr, pIndex + 1, high);
+        }
+    }
+    static int partition(int arr[], int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (i <= high - 1 && arr[i] <= pivot) {
+                i++;
+            }
+            while (j >= low + 1 && arr[j] > pivot) {
+                j--;
+            }
+            if (i < j) {
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, low, j); 
+        return j;
+    }
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
