3c0000766f8eabac3e77217b764ac84980ed40fb
Pranayande <165485309+Pranayande@users.noreply.github.com>
2025-07-10 10:03:51 +0530

    Create 42. Trapping Rain Water.java
    This Java program solves the classic "Trapping Rain Water" problem using a space-optimized two-pointer approach. It calculates how much water can be trapped between bars of different heights after rainfall. By maintaining left and right maximums while traversing the array from both ends, this solution achieves linear time complexity O(n) and constant space complexity O(1). It’s a highly efficient method for solving this problem without using extra memory.

---
+++ 42. Trapping Rain Water.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int trap(int[] height) {
+        int lmax=0;
+        int rmax=0;
+        int total=0;
+        int left=0;
+        int right=height.length-1;
+        while(left<right){
+            if(height[left]<=height[right]){
+               if(lmax>height[left]){
+                total+=lmax-height[left];
+               }
+               else{
+                lmax=height[left];
+               }
+               left++;
+            }
+            else{
+                if(rmax>height[right]){
+                    total+=rmax-height[right];
+                }
+                else{
+                    rmax=height[right];
+                }
+                right--;
+            }
+            
+        }
+        return total;
+    }
+}
